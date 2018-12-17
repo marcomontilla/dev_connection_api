04f2329bb9590cf3ab1590a572aff76d20cd4c3d
Marco Montilla <marco.jr1695@gmail.com>
2018-12-17 14:45:51 -0500

    Dashboard Experience Display & Delete Section 10, Lecture 59 (In progress)

---
+++ client/src/components/dashboard/Experience.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import { withRouter, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+export class Experience extends Component {
+	render() {
+		return (
+			<div>
+				
+			</div>
+		)
+	}
+}
+
+
+
+export default connect(null)(withRouter(Experience))
