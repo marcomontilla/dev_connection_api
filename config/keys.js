9872493aa54082277aae1016d8f9b23799e56fa8
Marco Montilla <marco.jr1695@gmail.com>
2018-12-18 12:48:59 -0500

    Mongo Atlas DB Provider

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-	mongoURI: 'mongodb://marco:Marco1695@ds137483.mlab.com:37483/devconnmm',
+	// mongoURI: 'mongodb://marco:Marco1695@ds137483.mlab.com:37483/devconnmm', OLD URL
+	mongoURI: `mongodb://mmontil:adminpass@devconnectorcluster-shard-00-00-2485h.mongodb.net:27017,devconnectorcluster-shard-00-01-2485h.mongodb.net:27017,devconnectorcluster-shard-00-02-2485h.mongodb.net:27017/test?ssl=true&replicaSet=DevConnectorCluster-shard-0&authSource=admin&retryWrites=true`,
 	secretOrKey: 'secret',
 };
