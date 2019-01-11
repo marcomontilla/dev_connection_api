cf441f72a1e1d36506c23c9bf131df0271109299
Marco Montilla <marco.jr1695@gmail.com>
2019-01-10 20:02:30 -0500

    Keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-	// mongoURI: 'mongodb://marco:Marco1695@ds137483.mlab.com:37483/devconnmm', OLD URL
-	mongoURI: `mongodb://mmontil:adminpass@devconnectorcluster-shard-00-00-2485h.mongodb.net:27017,devconnectorcluster-shard-00-01-2485h.mongodb.net:27017,devconnectorcluster-shard-00-02-2485h.mongodb.net:27017/test?ssl=true&replicaSet=DevConnectorCluster-shard-0&authSource=admin&retryWrites=true`,
-	secretOrKey: 'secret',
-};
+if (process.env.NODE_ENV === 'production') {
+	module.exports = require('./keys_prod');
+} else {
+	module.exports = require('./keys_dev');
+}
